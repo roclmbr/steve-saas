f3aa792b3fd4e185df9e44d9c9a6bd78c015475e
Steve Adamson <mail@steveadamson.com>
2016-05-09 17:28:55 +0000

    Stripe, payments and update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -19,9 +19,7 @@ def create
 # make any changes
 
     tenant_params = sign_up_params_tenant
-    
-    user_params = sign_up_params_user
-    
+    user_params = sign_up_params_user.merge({ is_admin: true })
     coupon_params = sign_up_params_coupon
     
     sign_out_session!
